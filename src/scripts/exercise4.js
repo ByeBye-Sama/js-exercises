f3fc35bb3c860a95a3fc166d25a1932e235e66f6
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-04-10 13:09:34 -0500

    Finish peruvian temporal Wix

--- src/scripts/exercise4.js
+++ src/scripts/exercise4.js
@@ -1,18 +1,33 @@
 "use strict";
 
 function getInfo() {
-    let textInside = ''
+
+    let arrayElement = []
 
     let sendButton = document.querySelector(".js_sendButton");
     sendButton.addEventListener("click", function (e) {
         e.preventDefault();
-        textInside = document.querySelector(".js_textInside").value;
+        let htmlTag = document.querySelector(".js_htmlTag").value
+        let newHtml = document.createElement(htmlTag);
+        let textInside = document.querySelector(".js_textInside").value;
+        let text = document.createTextNode(textInside);
+        let classInside = document.querySelector(".js_classInside").value;
+        newHtml.setAttribute("class", classInside);
+        let styleInside = document.querySelector(".js_styleInside").value;
+        newHtml.setAttribute("style", styleInside);
+        let insideTag = document.querySelector(".js_selectElement").value;
 
-        let newText = `<p> ${textInside} </p>`;
-        document.querySelector('.js_newContent').innerHTML = newText;
-        textInside = '';
+        newHtml.appendChild(text);
+        document.querySelector(`.${insideTag}`).appendChild(newHtml); /* here change body */
+
+        let typeElement = document.createElement("option");
+        typeElement.setAttribute("value", classInside);
+        let textElement = document.createTextNode(classInside);
+        typeElement.appendChild(textElement);
+        document.querySelector(".js_selectElement").appendChild(typeElement);
 
     });
 }
 
 getInfo();
+
